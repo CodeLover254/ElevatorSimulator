6e782ab0198316d8ce4182c54c2988d82ee5d210
Patrick <pmwangi@bridgeinnovation.io>
2023-09-10 22:18:59 +0300

    chore: Add ElevatorControlException

---
+++ src/ElevatorSimulator.Domain/Exceptions/ElevatorControlException.cs
@@ -0,0 +1,9 @@
+﻿namespace ElevatorSimulator.Domain.Exceptions;
+
+public class ElevatorControlException: Exception
+{
+    public ElevatorControlException(string message):base(message)
+    {
+        
+    }
+}
