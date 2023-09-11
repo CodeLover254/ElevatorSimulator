7a41d8f1c5e79549c719c550eac895e4efe8efdd
Patrick <astroguypasa@gmail.com>
2023-09-11 16:59:34 +0300

    chore: Add configuration using appsettings and logging capabilities

--- src/ElevatorSimulator.Application/Services/ElevatorControlService.cs
+++ src/ElevatorSimulator.Application/Services/ElevatorControlService.cs
@@ -16,13 +16,13 @@ public class ElevatorControlService: IElevatorControlService
     public string GetElevatorStatus(string elevatorLabel)
     {
         var elevator = _building.Elevators[elevatorLabel];
-        if (elevator == null) throw new ElevatorControlException("Invalid elevator label");
+        if (elevator == null) throw new DomainException("Invalid elevator label");
         return elevator.GetStatus();
     }
 
     public IEnumerable<string> GetAllElevatorStatus()
     {
-        if (!_building.Elevators.Any()) throw new ElevatorControlException("No available elevators in the building");
+        if (!_building.Elevators.Any()) throw new DomainException("No available elevators in the building");
         return _building.Elevators.Select(e => e.Value.GetStatus());
     }
 
@@ -39,12 +39,12 @@ public class ElevatorControlService: IElevatorControlService
                 .ToList();
 
             if (!availableElevators.Any())
-                throw new ElevatorControlException("No available elevators to serve your request");
+                throw new DomainException("No available elevators to serve your request");
 
             var highestScoringElevator = availableElevators.First();
             var selectedElevator = _building.Elevators[highestScoringElevator.Label];
             var assignResult = selectedElevator.ReceiveRequest(request);
-            if (!assignResult) throw new ElevatorControlException("Unable to request elevator. Try again");
+            if (!assignResult) throw new DomainException("Unable to request elevator. Try again");
 
             return selectedElevator;
         });
