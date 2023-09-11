78cdb0688243461f1f95b611ebf48bc756165723
Patrick <pmwangi@bridgeinnovation.io>
2023-09-11 23:16:02 +0300

    chore: Changed comparer.

--- src/ElevatorSimulator.Domain/Comparers/RequestInverseComparer.cs
+++ src/ElevatorSimulator.Domain/Comparers/RequestInverseComparer.cs
@@ -6,8 +6,8 @@ public class RequestInverseComparer: IComparer<Request>
 {
     public int Compare(Request? x, Request? y)
     {
-        if (x.DestinationFloor == y.DestinationFloor) return 0;
-        if (x.DestinationFloor < y.DestinationFloor) return 1;
+        if (x.SourceFloor == y.SourceFloor) return 0;
+        if (x.SourceFloor < y.SourceFloor) return 1;
         return -1;
     }
 }
