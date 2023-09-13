7533395e051e8adad5568506458fbf523ac6f830
Patrick <astroguypasa@gmail.com>
2023-09-13 12:59:42 +0300

    chore: Add testing logic

--- tests/ElevatorSimulator.UnitTests/BaseElevatorTests.cs
+++ tests/ElevatorSimulator.UnitTests/BaseElevatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using ElevatorSimulator.Domain.Enums;
 using ElevatorSimulator.Domain.Models;
+using FluentAssertions;
 
 namespace ElevatorSimulator.UnitTests;
 
@@ -8,107 +9,135 @@ public class BaseElevatorTests
     private readonly BaseElevator _testSubject1;
     private readonly BaseElevator _testSubject2;
     private readonly BaseElevator _testSubject3;
-    private readonly Building _building;
+    private const int Floors = 20;
+    private const int Capacity = 10;
+    private const int Passengers = 5;
     public BaseElevatorTests()
     {
-        var floors = 20;
-        var capacity = 10;
+        
         _testSubject1 = new PassengerElevator
         {
             Label = "P1",
             CurrentFloor = 0,
-            HighestFloor = floors - 1,
+            HighestFloor = Floors - 1,
             LowestFloor = 0,
-            MaximumPassengers = capacity,
-            ElevatorType = ElevatorType.Passenger,
-            Passengers = 5
+            MaximumPassengers = Capacity,
+            Passengers = Passengers
         };
         
         _testSubject2 = new PassengerElevator
         {
             Label = "P2",
             CurrentFloor = 9,
-            HighestFloor = floors - 1,
+            HighestFloor = Floors - 1,
             LowestFloor = 0,
-            MaximumPassengers = capacity,
-            ElevatorType = ElevatorType.Passenger,
+            MaximumPassengers = Capacity,
         };
         
-        _testSubject2 = new FreightElevator()
+        _testSubject3 = new FreightElevator()
         {
             Label = "P2",
             CurrentFloor = 9,
-            HighestFloor = floors - 1,
+            HighestFloor = Floors - 1,
             LowestFloor = 0,
-            MaximumWeight= capacity,
-            ElevatorType = ElevatorType.Passenger,
-            CurrentWeight = capacity
-        };
-
-        var elevatorStore = new Dictionary<string, BaseElevator>();
-        elevatorStore[_testSubject1.Label] = _testSubject1;
-        elevatorStore[_testSubject2.Label] = _testSubject2;
-        elevatorStore[_testSubject3.Label] = _testSubject3;
-
-        _building = new Building
-        {
-            NumFloors = floors,
-            NumElevators = 3,
-            Elevators = elevatorStore
+            MaximumWeight= Capacity,
+            CurrentWeight = Capacity
         };
     }
 
     [Fact]
     public void IsFullyLoaded_ReturnsCorrectState()
     {
-        
+        var result1 = _testSubject1.IsFullyLoaded();
+        var result2 = _testSubject3.IsFullyLoaded();
+
+        result1.Should().BeFalse();
+        result2.Should().BeTrue();
     }
     
     [Fact]
     public void RemainingCapacity_ReturnsCorrectValue()
     {
-        
+        var expectedResult = Capacity - Passengers;
+        var result = _testSubject1.RemainingCapacity();
+        result.Should().Be(expectedResult);
     }
     
     [Fact]
     public void GetMaximumCapacity_ReturnsCorrectValue()
     {
-        
-    }
-
-    [Fact]
-    public void GetStatus_ReturnsCorrectElevatorStatus()
-    {
-        
+        var result = _testSubject2.GetMaximumCapacity();
+        result.Should().Be(Capacity);
     }
 
     [Fact]
     private void ModifyLoading_CorrectlyMutatesElevatorLoad()
     {
-        
+        var additionalPassengers = 3;
+        var totalPassegers = Passengers + additionalPassengers;
+        var expectedResult = Capacity - totalPassegers;
+        _testSubject1.ModifyLoading(additionalPassengers, ElevatorLoadingOptions.Add);
+        var result = _testSubject1.RemainingCapacity();
+        result.Should().Be(expectedResult);
     }
 
     [Fact]
     private void GetAvailabilityScore_ReturnsUnavailableForFullyLoadedElevator()
     {
-        
-    }
+        var result = _testSubject3.GetAvailabilityScore(new Request
+        {
+            RequestType = ElevatorType.Freight,
+            Capacity = 10
+        });
 
+        result.Should().Be((int)ElevatorAvailability.Unavailable);
+    }
+    
     [Fact]
-    public void DetermineDefaultDirection_ReturnsCorrectDirection()
+    private void GetAvailabilityScore_ReturnsUnavailableForFullyUnrelatedRequestType()
     {
-        
+        var result = _testSubject1.GetAvailabilityScore(new Request
+        {
+            RequestType = ElevatorType.Freight
+        });
+
+        result.Should().Be((int)ElevatorAvailability.Unavailable);
     }
 
     [Fact]
     public void ScheduleRequest_ReturnsCorrectBoardingNumber()
     {
-        
+        var boardingPassengers = 3;
+        var expectedResult = Math.Min(_testSubject1.RemainingCapacity(),boardingPassengers);
+        var result = _testSubject1.ScheduleRequest(new Request
+        {
+            Capacity = boardingPassengers,
+            DestinationFloor = 10,
+            RequestType = ElevatorType.Passenger,
+            SourceFloor = 0
+        });
+
+        result.Should().Be(expectedResult);
     }
     
     [Fact]
-    public void Run_SuccessfullyRunsElevatorProcesses()
+    public async Task Run_SuccessfullyRunsElevatorProcesses()
     {
+        var expectedResult = 12;
         
+        _testSubject2.ScheduleRequest(new Request
+        {
+            Capacity = 2,
+            DestinationFloor = expectedResult,
+            RequestType = ElevatorType.Passenger,
+            SourceFloor = 9
+        });
+
+        await _testSubject2.ActivateAsync();
+        
+        Thread.Sleep(10000); //sleep to allow elevator to arrive.
+
+        var currentFloor = _testSubject2.CurrentFloor;
+        currentFloor.Should().Be(expectedResult);
     }
 }
